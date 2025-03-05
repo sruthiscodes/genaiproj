57af6926d81137e5a08d86ecc839b0ddcd1696c0
Sushmita Sree S <133584049+suree20@users.noreply.github.com>
2025-03-05 10:33:44 +0530

    Delete Sushmita directory

--- Sushmita/Semester3/wt/nimbleapp/client/src/axiosConfig.js
+++
@@ -1,4 +0,0 @@
-import axios from 'axios';
-
-axios.defaults.baseURL =
-  process.env.NODE_ENV !== 'production' ? 'http://localhost:5000' : '/';
--- Sushmita/Semester3/wt/nimbleapp/server/src/constants/httpStatus.js
+++
@@ -1,2 +0,0 @@
-export const BAD_REQUEST = 400;
-export const UNAUTHORIZED = 401;
